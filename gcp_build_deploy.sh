87464c96223348fc28492f88c24d7620a868463b
WilliSantos <willi.santos@infoprice.co>
2020-05-12 22:07:37 -0300

    Fix scripts for k8s

--- gcp_build_deploy.sh
+++ gcp_build_deploy.sh
@@ -4,6 +4,10 @@
 ### BUILD JAVA ####
 ###################
 
+setting_variables () {
+    sed -i "s/IMAGE_VERSION/${IMAGE_VERSION}/g" ./deployment.yaml
+}
+
 build_and_tag_docker () {
     docker build -t prd-matrix -f Dockerfile .
     docker tag prd-matrix:latest gcr.io/devops-273118/matrix:${IMAGE_VERSION}
@@ -31,10 +35,12 @@ PIPELINE_STATUS=$1
 
 case ${PIPELINE_STATUS} in
     build)
+        setting_variables
         build_and_tag_docker
         docker_push
         ;;
     deploy)
+        setting_variables
         build_and_tag_docker
         docker_push
         update_deployment
